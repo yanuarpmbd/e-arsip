b1c39d1e40195972c661b61629c164fe1f5cea08
bidangpdi <pdi.dpmptspjateng@gmail.com>
2020-09-29 15:24:57 +0700

    fix download button

--- resources/views/vendor/voyager/arsips/browse.blade.php
+++ resources/views/vendor/voyager/arsips/browse.blade.php
@@ -188,13 +188,13 @@
                                                         @include('voyager::multilingual.input-hidden-bread-browse')
                                                         @if(json_decode($data->{$row->field}) !== null)
                                                             @foreach(json_decode($data->{$row->field}) as $file)
-                                                                <a href="{{ Storage::disk(config('voyager.storage.disk'))->url($file->download_link) ?: '' }}" target="_blank">
+                                                                <a href="{{ Storage::disk(config('voyager.storage.disk'))->url($file->download_link) ?: '' }}" target="_blank" class="btn btn-sm btn-success">
                                                                     Download
                                                                 </a>
                                                                 <br/>
                                                             @endforeach
                                                         @else
-                                                            <a href="{{ Storage::disk(config('voyager.storage.disk'))->url($data->{$row->field}) }}" target="_blank">
+                                                            <a href="{{ Storage::disk(config('voyager.storage.disk'))->url($data->{$row->field}) }}" target="_blank" class="btn btn-sm btn-success">
                                                                 Download
                                                             </a>
                                                         @endif
@@ -343,13 +343,13 @@
                                                         @include('voyager::multilingual.input-hidden-bread-browse')
                                                         @if(json_decode($data->{$row->field}) !== null)
                                                             @foreach(json_decode($data->{$row->field}) as $file)
-                                                                <a href="{{ Storage::disk(config('voyager.storage.disk'))->url($file->download_link) ?: '' }}" target="_blank">
+                                                                <a href="{{ Storage::disk(config('voyager.storage.disk'))->url($file->download_link) ?: '' }}" target="_blank" class="btn btn-sm btn-success">
                                                                     Download
                                                                 </a>
                                                                 <br/>
                                                             @endforeach
                                                         @else
-                                                            <a href="{{ Storage::disk(config('voyager.storage.disk'))->url($data->{$row->field}) }}" target="_blank">
+                                                            <a href="{{ Storage::disk(config('voyager.storage.disk'))->url($data->{$row->field}) }}" target="_blank" class="btn btn-sm btn-success">
                                                                 Download
                                                             </a>
                                                         @endif
