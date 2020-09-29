c4c201cca402f5a634c0c5c8882cc0e22f995b67
bidangpdi <pdi.dpmptspjateng@gmail.com>
2020-09-29 16:50:53 +0700

    add scope

--- app/Models/Arsip.php
+++ app/Models/Arsip.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Arsip extends Model
 {
     use HasFactory;
+
+    public function scopeArsip($query){
+        if(Auth::user()->hasRole('admin_kabkota')){
+            return $query->where('kabkota_id', Auth::user()->kabkota_id);
+        }
+        elseif(Auth::user()->hasRole('admin_skpd')) {
+            return $query->where('sektor_id', Auth::user()->sektor_id);
+        }
+        else{
+            return $query;
+        }
+    }
 }
