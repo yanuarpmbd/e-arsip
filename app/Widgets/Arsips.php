24f0eb8ef8a59f65250ec479bdac94b54c4face5
bidangpdi <pdi.dpmptspjateng@gmail.com>
2020-09-30 09:53:53 +0700

    add widgets

---
+++ app/Widgets/Arsips.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Widgets;
+
+use App\Models\Arsip;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+use TCG\Voyager\Facades\Voyager;
+use TCG\Voyager\Widgets\BaseDimmer;
+
+class Arsips extends BaseDimmer
+{
+    /**
+     * The configuration array.
+     *
+     * @var array
+     */
+    protected $config = [];
+
+    /**
+     * Treat this method as a controller action.
+     * Return view() or other content to display.
+     */
+    public function run()
+    {
+        $count = Arsip::all()->count();
+        $string = 'Arsips';
+
+        return view('voyager::dimmer', array_merge($this->config, [
+            'icon'   => 'voyager-window-list',
+            'title'  => "{$count} {$string}",
+            'text'   => __('voyager::dimmer.user_text', ['count' => $count, 'string' => Str::lower($string)]),
+            'button' => [
+                'text' => 'View all arsips',
+                'link' => route('voyager.arsips.index'),
+            ],
+            'image' => voyager_asset('images/widget-backgrounds/02.jpg'),
+        ]));
+    }
+
+    /**
+     * Determine if the widget should be displayed.
+     *
+     * @return bool
+     */
+    public function shouldBeDisplayed()
+    {
+        return Auth::user()->can('browse', Voyager::model('User'));
+    }
+}
